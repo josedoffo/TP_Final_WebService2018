9b169f318fd53984b816c46c382f6946b43b36f2
josedoffo <josedoffo@yahoo.com.ar>
2018-06-30 16:05:32 -0300

    NavBar + Enrutado

--- Tp/src/app/app-routing.module.ts
+++ Tp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {Punto1Component} from './Components/punto1/punto1.component';
+import {Punto2Component} from './Components/punto2/punto2.component';
+const routes: Routes = [
+  {path: 'punto1', component:Punto1Component},
+  {path: 'punto2',component:Punto2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Tp/src/app/app.module.ts
+++ Tp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { Punto1Component } from './Components/punto1/punto1.component';
+import { Punto2Component } from './Components/punto2/punto2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Punto1Component,
+    Punto2Component
   ],
   imports: [
     BrowserModule,
