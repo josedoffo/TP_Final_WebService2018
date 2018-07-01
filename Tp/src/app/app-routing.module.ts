ce0fc688e884f0749f475c540c70592582f44f5f
francobustamante <francobus1412@gmail.com>
2018-07-01 19:35:52 -0300

    Prueba

--- Tp/src/app/app-routing.module.ts
+++ Tp/src/app/app-routing.module.ts
@@ -3,22 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {Punto1Component} from './Components/punto1/punto1.component';
 import {Punto2Component} from './Components/punto2/punto2.component';
 import {HomeComponent} from './Components/home/home.component';
-<<<<<<< HEAD
-=======
-
->>>>>>> 648de94f92b3eb33da55a1cf825505fe2451f3e9
 import {LoginComponent} from './Components/login/login.component';
 
 
 const routes: Routes = [
   {path: 'punto1', component:Punto1Component},
   {path: 'punto2',component:Punto2Component},
-<<<<<<< HEAD
-  {path: 'login',component:LoginComponent}
-=======
   {path: 'login',component:LoginComponent},
   {path: 'home',component:HomeComponent}
->>>>>>> 648de94f92b3eb33da55a1cf825505fe2451f3e9
 ];
 
 
